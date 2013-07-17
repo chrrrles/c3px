155f4ade711d27eb36ddece6b2ede30f6bfc469f
charles.paul@gmail.com <charles.paul@gmail.com>
2013-07-17 18:28:02 +0000

    more golf with skeinforge

--- lib/skein.py
+++ lib/skein.py
@@ -1,16 +1,15 @@
 from  lib.skeinforge.skeinforge_application.skeinforge_plugins.analyze_plugins.statistic import StatisticRepository
 from  lib.skeinforge.skeinforge_application.skeinforge_plugins.analyze_plugins.statistic import StatisticSkein
+from lib.skeinforge.skeinforge_application.skeinforge_plugins.craft_plugins.export import writeOutput
+from lib.skeinforge.skeinforge_application.skeinforge_plugins.craft_plugins.export import ExportRepository
+from lib.skeinforge.skeinforge_application.skeinforge_utilities import skeinforge_craft
+from lib.skeinforge.fabmetheus_utilities import settings
+
 #from lib.skeinforge.fabmetheus_utilities import writeOutput
-
-
 repository = StatisticRepository()
 repository.density = 930.0
 repository.material = 20.0
 
-
-def skein(filename):
-  gcode = gcodec.getGcodeFileText(filename,' ')
-  gfile ="%s.gcode" % filename 
-  statistics = StatisticSkein().getCraftedGcode(gcode, repository )
-  print statistics
+def skein():
+  pass
 
