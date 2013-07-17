66f8753eaa5e423ad987a3d62ec4903232b1b12b
charles.paul@gmail.com <charles.paul@gmail.com>
2013-07-17 18:28:21 +0000

    adding serialized names

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from schematics.models import Model
-from schematics.types import StringType, FloatType,EmailType
+from schematics.types import StringType,EmailType, IntType
 from schematics.exceptions  import ValidationError
 
 #from lib.cc import country_codes
@@ -13,29 +13,42 @@ class TestModel(Model):
 class RfpModel(Model):
   email = EmailType ( required=True) 
   email.max_length = 40
+  email.serialized_name = "Email"
 
   firstname = StringType ( required=True)
   firstname.max_length=40
+  firstname.serialized_name = "First Name"
 
   lastname = StringType ( required=True)
   lastname.max_length=40
+  lastname.serialized_name = "Last Name"
 
   street1 = StringType (required=True)  
   street1.max_length=40
+  street1.serialized_name = "Street 1"
 
   street2 = StringType ()
   street2.max_length=40
+  street2.serialized_name = "Street 2"
 
   city = StringType ( required=True)
   city.max_length=40
+  city.serialized_name = "City"
 
   state = StringType ( required=True)
   state.max_length=2
+  state.serialized_name = "State"
+
+  file_id = IntType( required=True)
+  file_id.serialized_name = "File"
+
+  postal_code = StringType ( required=True)
+  postal_code.max_length=10
+  postal_code.serialized_name = "Zip/Postal Code"
 
   country = StringType ( required=True, default="US", choices=country_codes)
+  country.serialized_name = "Country"
 
-  final_filename =  StringType()
-  final_filename.hidden = True
 
 class BuyerModel(Model):
   email = StringType ( required=True) 
