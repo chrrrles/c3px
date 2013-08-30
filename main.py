7c61f2169861b2871b9170a543004da41874e7f4
thethethe <thethethe>
2013-08-30 21:29:13 +0000

    changing how handlers uimodules are included

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from  motor import MotorClient
 import os
 
 from app import uimodules
-from app.handlers import RfpHandler, UploadHandler
+from app.handlers import RfpHandler, UploadHandler, HomeHandler
 
-static_path =os.path.join(os.getcwd(), 'app/lib/static')
+cwd = os.getcwd()
+static_path =os.path.join(cwd, 'app/lib/static')
+template_path = os.path.join(cwd, "app/templates")
 debug=True,
 routes = [ 
-  ('/',RfpHandler), 
+  ('/',HomeHandler), 
+  ('/create',RfpHandler), 
   ('/upload/', UploadHandler),
   ('/static/(.*)',tornado.web.StaticFileHandler, {'path':static_path})]
 db = MotorClient().open_sync().c3px
 
 
-application = tornado.web.Application(routes,db = db,ui_modules=uimodules, debug=debug, upload_dir="uploads") 
+application = tornado.web.Application(routes,db = db,ui_modules=uimodules, debug=debug, upload_dir="uploads", template_path=template_path) 
 if __name__ == "__main__":
   tornado.httpserver.HTTPServer(application).listen(8888)
   tornado.ioloop.IOLoop.instance().start()
