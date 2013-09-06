181bfde0091dd77d6886051b2ab22e3da10aca37
thethethe <thethethe>
2013-09-06 03:24:13 +0000

    making symlinks with the python fabfile to accomodate pil

--- fabfile/python.py
+++ fabfile/python.py
@@ -24,7 +24,14 @@ def install():
   install_virtualenv()
 
 def install_prerequisites(): # we need to get PIL prequisites...
-  package_ensure("python-dev")
+  package_ensure(prequisites)
+
+  # this is annoying -- PIL is hardcoded to search in /usr/lib for prequisites...
+  from os.path import join
+  src = '/usr/lib/i386-linux-gnu'
+  dst = '/usr/lib'
+  for f in ['libfreetype.so', 'libjpeg.so', 'libz.so']:
+    file_link(join(src,f), join(dst,f))
 
 def install_pip():
   package_ensure("python-pip")
