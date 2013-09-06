ecdc8fb405f56ddce67f480c02d774ff600a3dc0
thethethe <thethethe>
2013-09-06 02:57:58 +0000

    updating prequisites to support PIL in python fabfile

--- fabfile/python.py
+++ fabfile/python.py
@@ -4,6 +4,9 @@ from fabric.contrib.console import confirm
 from fabric.context_managers import *
 import string
 
+prequisites = ['build-essential', 'python-dev', 'libpng-dev',  
+  'libbz2-dev', 'zlib1g-dev', 'libfreetype6-dev', 'libjpeg8-dev']
+
 def ensure():
   if not is_installed():
    puts("Installing python support...")
@@ -20,7 +23,7 @@ def install():
   install_pip()
   install_virtualenv()
 
-def install_prerequisites():
+def install_prerequisites(): # we need to get PIL prequisites...
   package_ensure("python-dev")
 
 def install_pip():
